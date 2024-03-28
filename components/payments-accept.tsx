c8c137631f853586996da75b4feb61d8c0bc2a8a
Levix <1176891456@qq.com>
2024-03-28 11:51:08 +0800

    style:reduce duplicated code

--- components/payments-accept.tsx
+++ components/payments-accept.tsx
@@ -1,61 +1,28 @@
 import Image from 'next/image';
 
 const payments = [
-  {
-    alt: 'paypal',
-    src: 'paypal.png',
-  },
-  {
-    alt: 'visa',
-    src: 'visa.png',
-  },
-  {
-    alt: 'mastercard',
-    src: 'mastercard.png',
-  },
-  {
-    alt: 'american express',
-    src: 'american-express.png',
-  },
-  {
-    alt: 'discover',
-    src: 'discover.png',
-  },
-  {
-    alt: 'maestro',
-    src: 'maestro.png',
-  },
-  {
-    alt: 'diners club',
-    src: 'diners-club.png',
-  },
-  {
-    alt: 'jcb',
-    src: 'jcb.png',
-  },
-  {
-    alt: 'cb',
-    src: 'cb.png',
-  },
-  {
-    alt: 'apple pay',
-    src: 'apple-pay.png',
-  },
-  {
-    alt: 'google pay',
-    src: 'google-pay.png',
-  },
+  'paypal',
+  'visa',
+  'mastercard',
+  'american-express',
+  'discover',
+  'maestro',
+  'diners-club',
+  'jcb',
+  'cb',
+  'apple-pay',
+  'google-pay',
 ];
 
 const PaymentsAccept = () => {
   return (
     <div className="flex">
-      {payments.map(({ alt, src }) => (
+      {payments.map((name) => (
         <Image
-          key={alt}
+          key={name}
           className="mr-2 mb-2"
-          src={`/payments/${src}`}
-          alt={alt}
+          src={`/payments/${name}.png`}
+          alt={name}
           width={42}
           height={28}
         />
