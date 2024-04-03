ff21c2a775e403a4c0098e7f0857f017de798ff9
Levix <1176891456@qq.com>
2024-04-03 11:03:32 +0800

    feat:add doc link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -54,7 +54,28 @@ const Header = async () => {
             <CategoriesNav categories={categories} />
           </div>
 
-          <div className="max-[639px]:flex-1 flex justify-end space-x-3">
+          <div className="max-[639px]:flex-1 flex justify-end space-x-2">
+            <a
+              href="https://doc.next-woo.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-black text-sm leading-6 text-[#666] flex"
+            >
+              <span>Doc</span>
+              <svg
+                aria-hidden="true"
+                className=""
+                height="7"
+                viewBox="0 0 6 6"
+                width="7"
+              >
+                <path
+                  d="M1.25215 5.54731L0.622742 4.9179L3.78169 1.75597H1.3834L1.38936 0.890915H5.27615V4.78069H4.40513L4.41109 2.38538L1.25215 5.54731Z"
+                  fill="#999"
+                ></path>
+              </svg>
+            </a>
+
             <a
               href="https://github.com/Levix0501/next-woo"
               target="_blank"
